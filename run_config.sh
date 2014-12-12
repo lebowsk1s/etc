27f42e88fe0dfae2910ef9eb4064e0c755839268
leo <xxgghh@gmail.com>
2014-12-12 11:15:39 +0800

    modify 2 file

--- run_config.sh
+++ run_config.sh
@@ -1,21 +1,44 @@
 #!/bin/bash
 
 # setting the vimrc in your home
+echo "Set the .vimrc in your home"
 if [ -f "$HOME/.vimrc" ]; then
-    for ((i=0; i<10; ++i)) 
+    for ((i=0; i<10; ++i))
     do
         VIMRC_BAK=$HOME/.vimrc.bak.$i
         if [ ! -f $VIMRC_BAK ]; then
-            break 
+            break
         fi
     done
     echo "The .vimrc file is exist, rename it to $VIMRC_BAK"
     mv $HOME/.vimrc $VIMRC_BAK
 fi
-echo "Set the .vimrc in your home"
-cp .vimrc $HOME/.vimrc
+cp -v .vimrc $HOME/.vimrc
 
+# setting the bash aliases in your home
+echo "Set the .bash_aliases in your home"
+if [ -f "$HOME/.bash_aliases" ]; then
+    for ((i=0; i<10; ++i))
+    do
+        ALIASES_BAK=$HOME/.bash_aliases.bak.$i
+        if [ ! -f $ALIASES_BAK ]; then
+            break
+        fi
+    done
+    echo "The .bash_aliases file is exist, rename it to $ALIASES_BAK"
+    mv $HOME/.bash_aliases $ALIASES_BAK
+fi
+cp -v .bash_aliases $HOME/.bash_aliases
+. .bash_aliases
 
+type foo >/dev/null 2>&1
+
+if [ $? == 0 ]; then
+    echo "Set the tmux config file in your home"
+    cp .tmux.conf $HOME/.tumux.conf
+else
+    echo "You don't have tmux, needn't to set it"
+fi
 
 
 #测试权限
